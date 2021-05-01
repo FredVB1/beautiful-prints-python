16cbbbd8307a4ca6211ebb738f2ecf63a14d901c
account.frederic.vb@web.de <account.frederic.vb@web.de>
2021-05-01 14:54:29 +0200

    Finished version.

--- printNestedData.py
+++ printNestedData.py
@@ -1,10 +1,68 @@
 
-def printNestedData(nestedData: any, maxItemsPerLine: int = 5, indent: int = 4) -> str:
 
-    return _printNestedData(nestedData, indent, maxItemsPerLine)
+def printNestedData(nestedData: any, maxItemsPerLine: int = 5, indent: int = 4):
+    """
+    Outputs your passed [nestedData] in a beautiful way.
+
+        The Concept ist that if an item of the nested data is a nested data type or the length of the nested data is
+        bigger than the maximum items per line [maxItemsPerLine], it will be put in the next line.
+
+        The information that an item of the nested data is a nested data type, is more important than the maximum items
+        per line [maxItemsPerLine]. Therefore [maxItemsPerLine] does not specify the minimum of items per line.
+
+    Currently supported nested data types:
+        tuple,
+        dict,
+        list,
+        set,
+        frozenset
+
+    No error will be raised if your nested data is not contained in the currently supported data types.
 
 
-def _printNestedData(nestedData: any, indent: int,  maxItemsPerLine: int = 5, depth: int = 0) -> str:
+    Args:
+        nestedData (any): The nested data you want to print out in a beautiful way.
+        maxItemsPerLine (int, optional): Maximum items allowed per line (default = 5).
+        indent (int): Indentation with which the data should be formatted.
+    """
+    import sys
+
+    sys.stdout.write(_generateBeautifulString(nestedData, indent, maxItemsPerLine))
+
+
+def generateBeautifulString(nestedData: any, maxItemsPerLine: int = 5, indent: int = 4) -> str:
+    """
+    Generates a beautifully formatted string containing the passed [nestedData], so you can print nested data in a
+    beautiful way to the console.
+
+        The Concept ist that if an item of the nested data is a nested data type or the length of the nested data is
+        bigger than the maximum items per line [maxItemsPerLine], it will be put in the next line.
+
+        The information that an item of the nested data is a nested data type, is more important than the maximum items
+        per line [maxItemsPerLine]. Therefore [maxItemsPerLine] does not specify the minimum of items per line.
+
+    Currently supported nested data types:
+        tuple,
+        dict,
+        list,
+        set,
+        frozenset
+
+    No error will be raised if your nested data is not contained in the currently supported data types.
+
+
+    Args:
+        nestedData (any): The nested data you want to print out in a beautiful way.
+        maxItemsPerLine (int, optional): Maximum items allowed per line (default = 5).
+        indent (int): Indentation with which the data should be formatted.
+    """
+
+    return _generateBeautifulString(nestedData, indent, maxItemsPerLine)
+
+
+def _generateBeautifulString(
+        nestedData: any, indent: int,  maxItemsPerLine: int = 5, depth: int = 0, isDict: bool = False) -> str:
+
     nestedDataTypes = {
         tuple: ("(", ")"),
         dict: ("{", "}"),
@@ -19,41 +77,51 @@ def _printNestedData(nestedData: any, indent: int,  maxItemsPerLine: int = 5, de
     for index, data in enumerate(nestedData):
         done = False
         if type(nestedData) == dict:
-            if type(nestedData[data]) in nestedDataTypes or (type(nestedData[data]) in nestedDataTypes and len(nestedData[data]) > maxItemsPerLine):
-                for subData in nestedData[data]:
-                    if done is False:
-                        if type(subData) in nestedDataTypes or (type(nestedData[data]) in nestedDataTypes and len(nestedData[data]) > maxItemsPerLine):
-                            output += _printNestedData(data, indent, maxItemsPerLine, depth=depth + 1)
-                            done = True
-                            break
-            if done is False:
-                if index == 0:
-                    output += f"\n{spacing * depth}{nestedDataTypes[type(nestedData)][0]}"
-                else:
-                    output += ","
+            if index == 0:
+                output += f"\n{spacing * depth}{nestedDataTypes[type(nestedData)][0]}"
+            else:
+                output += ","
 
-                output += f"""\n{spacing * (depth + 1)}""" \
-                          f""" {f'"{data}"' if type(data) == str else data}:""" \
-                          f""" {f'"{nestedData[data]}"' if type(nestedData[data]) == str else nestedData[data]}"""
+            output += f"""\n{spacing * (depth + 1)}""" \
+                      f""" {f'"{data}"' if type(data) == str else data}:"""
+
+            if type(nestedData[data]) in nestedDataTypes:
+                if hasattr(nestedData[data], '__iter__'):
+                    for subData in nestedData[data]:
+                        if done is False:
+                            if type(subData) in nestedDataTypes or \
+                                    (type(nestedData[data]) in nestedDataTypes and len(nestedData[data]) > maxItemsPerLine):
+                                output += _generateBeautifulString(
+                                    nestedData[data],
+                                    indent,
+                                    maxItemsPerLine,
+                                    depth=depth + 1,
+                                    isDict=True
+                                )
+                                done = True
+                                break
+
+            if done is False:
+                output += f""" {f'"{nestedData[data]}"' if type(nestedData[data]) == str else nestedData[data]}"""
 
         else:
-            if type(data) in nestedDataTypes or (type(data) in nestedDataTypes and len(data) > maxItemsPerLine):
-                done = False
-                for subData in data:
-                    if done is False:
-                        if type(subData) in nestedDataTypes or (type(data) in nestedDataTypes and len(data) > maxItemsPerLine):
-                            output += ","
-                            output += _printNestedData(data, indent, maxItemsPerLine, depth=depth + 1)
-                            done = True
-                            break
+            if index == 0:
+                output += (f"\n{spacing * depth}" if not isDict else f" ") + \
+                          f"{nestedDataTypes[type(nestedData)][0]}"
+            else:
+                output += ","
+
+            if type(data) in nestedDataTypes:
+                if hasattr(data, '__iter__'):
+                    for subData in data:
+                        if done is False:
+                            if type(subData) in nestedDataTypes or \
+                                    (type(data) in nestedDataTypes and len(data) > maxItemsPerLine):
+                                output += _generateBeautifulString(data, indent, maxItemsPerLine, depth=depth + 1)
+                                done = True
+                                break
 
             if done is False:
-                if index == 0:
-                    output += f"\n{spacing * depth}" \
-                              f"{nestedDataTypes[type(nestedData)][0]}"
-                else:
-                    output += ","
-
                 output += f"""\n{spacing * (depth + 1)}""" \
                           f"""{f'"{data}"' if type(data) == str else data}"""
 
